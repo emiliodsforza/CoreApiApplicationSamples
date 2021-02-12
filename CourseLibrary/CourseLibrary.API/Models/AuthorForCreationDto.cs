b873a228db41bc35b095ba781096efb39162a79e
Emilio D Sforza <esforza@emiliodsforza.com>
2021-02-12 08:24:40 -0500

    Corrections for Mod 6

--- CourseLibrary/CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -8,7 +8,7 @@ namespace CourseLibrary.API.Models
     public class AuthorForCreationDto
     {
         public string FirstName { get; set; }
-        public string Lastname { get; set; }
+        public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string MainCategory { get; set; }
         public ICollection<CourseForCreationDto> Courses { get; set; }
